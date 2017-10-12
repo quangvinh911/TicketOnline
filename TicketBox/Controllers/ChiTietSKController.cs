95f611a332084202b17faaef7e159a2ea7fca3f1
leson96 <lenguyenhuynhson29@gmail.com>
2017-10-12 13:50:21 +0700

    mailchimp

---
+++ TicketBox/Controllers/ChiTietSKController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicketBox.Models;
+
+namespace TicketBox.Controllers
+{
+    public class ChiTietSKController : Controller
+    {
+        //
+        // GET: /ChiTietSK/
+        TicketOnlineEntities db = new TicketOnlineEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult ChiTietSK(string maSuKien)
+        {
+            SuKien sukien = db.SuKiens.SingleOrDefault(n => n.MaSK == maSuKien);
+            if (sukien == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sukien);
+        }
+    }
+}
